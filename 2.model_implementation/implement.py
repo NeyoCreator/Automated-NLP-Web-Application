1d23794ea5939354ce702df746288553caabb814
Neo Sebanze <neo@bidvestalice.com>
2022-06-29 05:00:26 +0200

    FIXED THE RESULT APPLICATION

--- 2.model_implementation/implement.py
+++ 2.model_implementation/implement.py
@@ -2,7 +2,7 @@ import pickle
 pkl_file = "model.pkl"  
 with open(pkl_file, 'rb') as file:  
     pickle_model = pickle.load(file)
-new_input = 'i just love south africa'
+new_input = 'Ngiyamthanda umama wami kakhulu'
 predictions = pickle_model.predict([new_input])
 print(predictions)
 
