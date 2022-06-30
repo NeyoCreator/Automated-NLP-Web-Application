45887d2687ea47afa76cd0925b691c38d48f11aa
Neo Sebanze <neo@bidvestalice.com>
2022-06-30 05:55:41 +0200

    CREATED UPLOAD TEXT FILE

--- 2.model_implementation/main.py
+++ 2.model_implementation/main.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash
+from wtforms import StringField, PasswordField,BooleanField,IntegerField
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Email, Length 
+import os
+from flask_bootstrap import Bootstrap
+
 
 from flask import Flask
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY']=SECRET_KEY
+bootstrap = Bootstrap(app)
+
+class InputForm(FlaskForm):
+    username = StringField('username',validators = [InputRequired(), Length(min=4, max=15 )])
+
 
 @app.route("/")
 def home():
-    return render_template('index.html')
-
+    form = InputForm()
+    flash(f" ZA tokens has been sent to")
+    return render_template('index.html',form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
