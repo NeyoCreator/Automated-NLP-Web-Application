41808c642c931172d0728f2c15e02b7740ecfb67
Neo Sebanze <neo@bidvestalice.com>
2022-06-30 06:45:54 +0200

    MANAGED TO IDENTIFY THE LANGUAGE

--- 2.model_implementation/main.py
+++ 2.model_implementation/main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template,flash
+from flask import Flask, render_template,flash,request
+from numpy import dtype
 from wtforms import StringField, PasswordField,BooleanField,IntegerField
 from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired, Email, Length 
 import os
 from flask_bootstrap import Bootstrap
+import pickle
 
 
 from flask import Flask
@@ -12,15 +14,21 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY']=SECRET_KEY
 bootstrap = Bootstrap(app)
 
-class InputForm(FlaskForm):
-    username = StringField('username',validators = [InputRequired(), Length(min=4, max=15 )])
 
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    form = InputForm()
-    flash(f" ZA tokens has been sent to")
-    return render_template('index.html',form = form)
+    pkl_file = "model.pkl"  
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = [request.form.get("fname")]
 
+       with open(pkl_file, 'rb') as file:  
+        pickle_model = pickle.load(file)
+        predictions = pickle_model.predict(first_name)
+
+       return "Your name is " + str(predictions)
+    return render_template("index.html")
 if __name__ == '__main__':
     app.run(debug=True)
